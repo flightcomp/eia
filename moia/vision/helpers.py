99d88adff797e9515f194637907226e1c5b9547b
roberto <theairdroid1@gmail.com>
2026-03-15 17:48:25 +0100

    Mediapipe

--- moia/vision/helpers.py
+++ moia/vision/helpers.py
@@ -53,3 +53,69 @@ def uri_validator(x):
         return all([result.scheme, result.netloc])
     except AttributeError:
         return False
+        
+from mediapipe.tasks.python.vision import drawing_utils
+from mediapipe.tasks.python.vision import drawing_styles
+from mediapipe.tasks.python import vision
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def draw_landmarks_on_image(rgb_image, detection_result):
+  face_landmarks_list = detection_result.face_landmarks
+  annotated_image = np.copy(rgb_image)
+
+  # Loop through the detected faces to visualize.
+  for idx in range(len(face_landmarks_list)):
+    face_landmarks = face_landmarks_list[idx]
+
+    # Draw the face landmarks.
+
+
+    drawing_utils.draw_landmarks(
+        image=annotated_image,
+        landmark_list=face_landmarks,
+        connections=vision.FaceLandmarksConnections.FACE_LANDMARKS_TESSELATION,
+        landmark_drawing_spec=None,
+        connection_drawing_spec=drawing_styles.get_default_face_mesh_tesselation_style())
+    drawing_utils.draw_landmarks(
+        image=annotated_image,
+        landmark_list=face_landmarks,
+        connections=vision.FaceLandmarksConnections.FACE_LANDMARKS_CONTOURS,
+        landmark_drawing_spec=None,
+        connection_drawing_spec=drawing_styles.get_default_face_mesh_contours_style())
+    drawing_utils.draw_landmarks(
+        image=annotated_image,
+        landmark_list=face_landmarks,
+        connections=vision.FaceLandmarksConnections.FACE_LANDMARKS_LEFT_IRIS,
+          landmark_drawing_spec=None,
+          connection_drawing_spec=drawing_styles.get_default_face_mesh_iris_connections_style())
+    drawing_utils.draw_landmarks(
+        image=annotated_image,
+        landmark_list=face_landmarks,
+        connections=vision.FaceLandmarksConnections.FACE_LANDMARKS_RIGHT_IRIS,
+          landmark_drawing_spec=None,
+          connection_drawing_spec=drawing_styles.get_default_face_mesh_iris_connections_style())
+
+  return annotated_image
+
+def plot_face_blendshapes_bar_graph(face_blendshapes):
+  # Extract the face blendshapes category names and scores.
+  face_blendshapes_names = [face_blendshapes_category.category_name for face_blendshapes_category in face_blendshapes]
+  face_blendshapes_scores = [face_blendshapes_category.score for face_blendshapes_category in face_blendshapes]
+  # The blendshapes are ordered in decreasing score value.
+  face_blendshapes_ranks = range(len(face_blendshapes_names))
+
+  fig, ax = plt.subplots(figsize=(12, 12))
+  bar = ax.barh(face_blendshapes_ranks, face_blendshapes_scores, label=[str(x) for x in face_blendshapes_ranks])
+  ax.set_yticks(face_blendshapes_ranks, face_blendshapes_names)
+  ax.invert_yaxis()
+
+  # Label each bar with values
+  for score, patch in zip(face_blendshapes_scores, bar.patches):
+    plt.text(patch.get_x() + patch.get_width(), patch.get_y(), f"{score:.4f}", va="top")
+
+  ax.set_xlabel('Score')
+  ax.set_title("Face Blendshapes")
+  plt.tight_layout()
+  plt.show()
